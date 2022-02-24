36690d03aa725502c9f85a81ae44b6cc179bd609
akash <akashingale62@gmail.com>
2022-02-24 18:28:45 +0530

    adag

--- blue/urls.py
+++ blue/urls.py
@@ -26,7 +26,6 @@ urlpatterns = [
     path("about/", views.About),
     path("services/", views.Service),
     path("contact/", views.Contact, name='Contact'),
-    # path("csrf_failure/", views.csrf_failure),
     path("project_delivered/", views.projectFuc)
 
 ]
